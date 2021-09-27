bf7cb34107fd8a26c673c983aa6e8535b7bb7eed
Melvin Omega <melvinomega151@gmail.com>
2021-09-27 09:00:11 +0300

    modify email

--- app/email.py
+++ app/email.py
@@ -1,7 +1,1 @@
-# def mail_message(subject,template,to,**kwargs):
-#     sender_email = 'irenekasiva75@gmail.com'
 
-#     email = Message(subject, sender=sender_email, recipients=[to])
-#     email.body= render_template(template + ".txt",**kwargs)
-#     email.html = render_template(template + ".html",**kwargs)
-#     mail.send(email)
